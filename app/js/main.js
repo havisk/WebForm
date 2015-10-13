b4bea9df51b4fc7f7fe1265da553bd15849dc742
Kool Havis <havisk@gmail.com>
2015-10-13 16:41:20 -0400

    got it to work

--- app/js/main.js
+++ app/js/main.js
@@ -2,5 +2,38 @@
 
 (function () {
 
-  console.log('It Works!');
+	var url = 'http://json-data.herokuapp.com/forms';
+
+	//fetch Data and do something with it
+	var promise = $.getJSON(url);
+
+	promise.then(function (data) {
+		fetch(data);
+	});
+
+	//template
+	var genericInput = _.template($('#generic-form').text());
+
+	// our do something
+	var fetch = function fetch(arr) {
+		_.each(arr, function (item) {
+
+			var htmlBlock = "";
+
+			if (item.type === 'text' || item.type === 'tel' || item.type === 'email') {
+				htmlBlock = genericInput(item);
+			}
+
+			$('form').append(htmlBlock);
+		});
+
+		// <div class="text-input for-elem">
+		// let textInput = function (obj){
+		// 	let template =`
+		// 	<input value "" type="${type}" placeholder="${label}" id="${obj.icon}"</i>
+		// 	</div>
+		// 	`;
+
+		// }
+	};
 })();
--- src/main.js
+++ src/main.js
@@ -1,5 +1,51 @@
-(function () {
+(function(){
+ 
+let url = 'http://json-data.herokuapp.com/forms';
 
-  console.log('It Works!');
+//fetch Data and do something with it
+let promise = $.getJSON(url);
 
-}());
+promise.then(function(data){
+fetch(data);
+});
+
+
+
+//template
+let genericInput = _.template($('#generic-form').text());
+
+
+
+// our do something
+let fetch = function (arr) {
+	_.each(arr, function (item) {
+
+	var htmlBlock = "";
+
+if (item.type === 'text'|| item.type === 'tel' || item.type ==='email') 
+{
+	htmlBlock = genericInput(item);
+}
+
+$('form').append(htmlBlock)
+
+});
+
+
+
+
+
+// <div class="text-input for-elem">
+// let textInput = function (obj){
+// 	let template =`
+// 	<input value "" type="${type}" placeholder="${label}" id="${obj.icon}"</i>
+// 	</div>
+// 	`;
+	
+// }
+
+
+};
+
+})();
+
